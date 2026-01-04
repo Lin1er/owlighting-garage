0fbc596476c522f5470fec127b79b3dc4b9fd864
lin1er <m.ulinasidiki@gmail.com>
2026-01-04 22:04:33 +0700

    Adjust positioning of animated element in Hero component for improved visibility

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -133,7 +133,7 @@ export default function Hero() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 1.2 }}
-          className="absolute bottom-10 left-1/2 -translate-x-1/2"
+          className="absolute left-1/2 -translate-x-1/2 translate-y-10"
         >
           <motion.div
             animate={{ y: [0, 10, 0] }}
